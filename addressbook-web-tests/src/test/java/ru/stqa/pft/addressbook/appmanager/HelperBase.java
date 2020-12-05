7cc1deff0b4ab1cf8dfd97ca82645320494ae3a5
StepanenkoIS <stepanenko.ilia@bk.ru>
2020-12-05 19:56:24 +0400

    Лекция 3.6. Оптимизация заполнения форм: оставляем не изменяющися значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -19,8 +19,11 @@ public class HelperBase {
   public void type(By locator, String text) {
     click(locator);
     if (text != null) {
-      wd.findElement(locator).clear();
-      wd.findElement(locator).sendKeys(text);
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (!existingText.equals(text)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
     }
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -11,7 +11,7 @@ public class ContactModificationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Grigory", "Pechorin", null));
+    app.getContactHelper().fillContactForm(new ContactData("Grigory", "Pechorin", "SPB"));
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().returnToHomePage();
   }
