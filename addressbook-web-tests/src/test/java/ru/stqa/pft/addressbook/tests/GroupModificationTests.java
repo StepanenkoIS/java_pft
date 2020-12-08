3d20f3ce7448ae93192afdad8b9615b1b5f79dbf
StepanenkoIS <stepanenko.ilia@bk.ru>
2020-12-08 20:27:36 +0400

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -11,25 +12,28 @@ public class GroupModificationTests extends TestBase{
   @Test
   public void testGroupModification() {
 
+
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
 
-    int before = app.getGroupHelper().getGroupCount();
     List<GroupData> beforeList = app.getGroupHelper().getGroupList();
 
     app.getGroupHelper().selectGroup(beforeList.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1-update", "test2-update", "test3-update"));
+    GroupData group = new GroupData(beforeList.get(beforeList.size()-1).getId(), "test1-update", "test2-update", "test3-update");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
 
-    int after = app.getGroupHelper().getGroupCount();
     List<GroupData> afterList = app.getGroupHelper().getGroupList();
-
-    Assert.assertEquals(after, before);
     Assert.assertEquals(afterList.size(), beforeList.size());
+
+    beforeList.remove(beforeList.size()-1);
+    beforeList.add(group);
+
+    Assert.assertEquals(new HashSet<Object>(afterList), new HashSet<Object>(beforeList));
 
   }
 
