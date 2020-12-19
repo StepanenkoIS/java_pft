d65ad5728b44d1be08e56aec044e686ad9d46060
StepanenkoIS <stepanenko.ilia@bk.ru>
2020-12-19 10:39:00 +0400

    Задание №11: Реализовать тест для проверки информации о контактах на главной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,21 +6,25 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
+  @Test(enabled = true)
   public void testContactCreation() throws Exception {
       app.goTo().gotoHomePage();
       Contacts before = app.contact().all();
-      ContactData contact = new ContactData()
+    File photo = new File("src/test/resources/stru.png");
+    ContactData contact = new ContactData()
               .withFirstName("Ivan")
               .withLastName("Oblomov")
               .withAddress("Saratov")
-              .withGroup("test1");
+              .withGroup("test1")
+              .withPhoto(photo);
       app.contact().create(contact, true);
       Contacts after = app.contact().all();
       Assert.assertEquals(after.size(), before.size() + 1);
@@ -29,5 +33,13 @@ public class ContactCreationTests extends TestBase {
               before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
+
+    public void testCurrentDir() {
+      File currentDir = new File(".");
+      System.out.println(currentDir.getAbsolutePath());
+      File photo = new File("src/test/resources/stru.png");
+      System.out.println(photo.getAbsolutePath());
+      System.out.println(photo.exists());
+    }
 }
 
