087968df7dcef15d1eb98dcf62644fd5ec762e2b
StepanenkoIS <stepanenko.ilia@bk.ru>
2020-12-24 16:24:43 +0400

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,12 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,32 +24,51 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test(enabled = true)
-  public void testContactCreation() throws Exception {
-      app.goTo().gotoHomePage();
-      Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData()
-              .withFirstName("Ivan")
-              .withLastName("Oblomov")
-              .withAddress("Saratov")
-              .withGroup("test1")
-              .withPhoto(photo);
-      app.contact().create(contact, true);
-      Contacts after = app.contact().all();
-      Assert.assertEquals(after.size(), before.size() + 1);
 
-      assertThat(after, equalTo(
-              before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validContactsJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
     }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType());
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
 
-
-    public void testCurrentDir() {
-      File currentDir = new File(".");
-      System.out.println(currentDir.getAbsolutePath());
-      File photo = new File("src/test/resources/stru.png");
-      System.out.println(photo.getAbsolutePath());
-      System.out.println(photo.exists());
+  @DataProvider
+  public Iterator<Object[]> validContactsXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
     }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+    return contacts.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+
+  @Test(dataProvider = "validContactsJson")
+  public void testContactCreation(ContactData contact) throws Exception {
+    app.goTo().gotoHomePage();
+    Contacts before = app.contact().all();
+    contact.withPhoto(new File(contact.getPhotoUrl()));
+    app.contact().create(contact, true);
+    Contacts after = app.contact().all();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }
+
+
 }
 
