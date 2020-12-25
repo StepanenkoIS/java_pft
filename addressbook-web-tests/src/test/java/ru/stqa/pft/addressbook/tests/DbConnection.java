60ea3e194419987d5048d7cfa457002e0613ba4a
StepanenkoIS <stepanenko.ilia@bk.ru>
2020-12-25 15:37:10 +0400

    Лекция 7.1. Драйвер базы данных. Получение данных из БД

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnection.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnection {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost/addressbook", "root", "");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      Groups groups = new Groups();
+
+      while (rs.next()) {
+        groups.add(new GroupData().withId(rs.getInt("group_id"))
+                .withName(rs.getString("group_name"))
+                .withHeader(rs.getString("group_header"))
+                .withFooter(rs.getString("group_footer")));
+      }
+
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
+
+
+    } catch (SQLException e) {
+      System.out.println("error -> "  + e.getMessage());
+    }
+  }
+}
